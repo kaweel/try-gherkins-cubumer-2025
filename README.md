9bf89f011697374e961558f178da382049e8fe0f
Kawee Lertrungmongkol <kaweelertrungmongkol@KAWEE-SRS.local>
2025-04-19 16:54:09 +0700

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # try-gherkins-cubumer-2025
+
+./mvnw clean test -Dcucumber.filter.tags="not @Skip"
---
+++ src/test/java/com/gherkin/try_gherkins_cubumer/TryGherkinsCubumerApplicationTest.java
@@ -0,0 +1,8 @@
+package com.gherkin.try_gherkins_cubumer;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TryGherkinsCubumerApplicationTest {
+    
+}
