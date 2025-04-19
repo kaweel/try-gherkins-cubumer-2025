1a5ad103fe8ec8bf86c6926154063657e4800eb2
Kawee Lertrungmongkol <kaweelertrungmongkol@KAWEE-SRS.local>
2025-04-19 16:55:55 +0700

    init

---
+++ src/main/java/com/gherkin/try_gherkins_cubumer/TryGherkinsCubumerApplication.java
@@ -0,0 +1,13 @@
+package com.gherkin.try_gherkins_cubumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryGherkinsCubumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryGherkinsCubumerApplication.class, args);
+	}
+
+}
